06707ea39677eb234a7b0d088a93624799769a8b
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2023-11-21 23:59:40 -0300

    separated by files

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    enabled = false,
+    "folke/flash.nvim",
+    ---@type Flash.Config
+    opts = {
+      search = {
+        forward = true,
+        multi_window = false,
+        wrap = false,
+        incremental = true,
+      },
+    },
+  },
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+}
